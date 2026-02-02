c6ebf39da7b3234e0ffedb2bc48a596d8e73a50d
Lunas1436 <naru1436t@gmail.com>
2026-02-02 21:41:10 +0900

    プロジェクトファイルを追加。

---
+++ MomoBlade/GameMain.cpp
@@ -0,0 +1,26 @@
+#include "GameMain.h"
+#include "DxLib.h"
+
+using namespace std;
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+    SetWindowText("MOMO BLADE"); // ウィンドウのタイトル
+    SetGraphMode(WIDTH, HEIGHT, 32); // ウィンドウの大きさとカラービット数の指定
+    ChangeWindowMode(TRUE); // ウィンドウモードで起動
+    if (DxLib_Init() == -1) return -1; // ライブラリ初期化 エラーが起きたら終了
+    SetBackgroundColor(0, 0, 0); // 背景色の指定
+    SetDrawScreen(DX_SCREEN_BACK); // 描画面を裏画面にする
+
+    while (1)
+    {
+        ClearDrawScreen(); // 画面クリア
+
+        ScreenFlip(); // 裏画面の内容を表画面に反映させる
+        if (ProcessMessage() == -1) break; // Windowsから情報を受け取りエラーが起きたら終了
+        if (CheckHitKey(KEY_INPUT_ESCAPE) == 1) break; // ESCキーが押されたら終了
+    }
+
+    DxLib_End(); // ＤＸライブラリ使用の終了処理
+    return 0;
+}
---
+++ MomoBlade/GameMain.h
@@ -0,0 +1,5 @@
+#pragma once
+
+const int HEIGHT = 480;
+const int WIDTH = 800;
+
